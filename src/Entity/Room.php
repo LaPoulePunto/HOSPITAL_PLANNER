c83212d661074ce90ee0d6a31ced68daf0c505d3
oscar <oscar.neveux@etudiant.univ-reims.fr>
2024-11-15 23:11:37 +0100

    Création de l'entité Room

---
+++ src/Entity/Room.php
@@ -0,0 +1,50 @@
+<?php
+
+namespace App\Entity;
+
+use App\Repository\RoomRepository;
+use Doctrine\ORM\Mapping as ORM;
+
+#[ORM\Entity(repositoryClass: RoomRepository::class)]
+class Room
+{
+    #[ORM\Id]
+    #[ORM\GeneratedValue]
+    #[ORM\Column]
+    private ?int $id = null;
+
+    #[ORM\Column]
+    private ?int $num = null;
+
+    #[ORM\Column(length: 3)]
+    private ?string $floor = null;
+
+    public function getId(): ?int
+    {
+        return $this->id;
+    }
+
+    public function getNum(): ?int
+    {
+        return $this->num;
+    }
+
+    public function setNum(int $num): static
+    {
+        $this->num = $num;
+
+        return $this;
+    }
+
+    public function getFloor(): ?string
+    {
+        return $this->floor;
+    }
+
+    public function setFloor(string $floor): static
+    {
+        $this->floor = $floor;
+
+        return $this;
+    }
+}
