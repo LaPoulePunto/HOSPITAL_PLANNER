941ea0029ffd6cda6f4e4028ae6b92d9ae8ca6d7
mart0495 <thibault.martin1@etudiant.univ-reims.fr>
2024-11-17 14:50:12 +0100

    Ajout de la condition, si un user n'est pas connecté, le rediriger vers la page de connexion

--- src/Controller/UserController.php
+++ src/Controller/UserController.php
@@ -2,7 +2,6 @@
 
 namespace App\Controller;
 
-use App\Entity\User;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
@@ -18,8 +17,11 @@ class UserController extends AbstractController
     }
 
     #[Route('/user/{id}', name: 'app_user_profile')]
-    public function profile(User $user): Response
+    public function profile(): Response
     {
-        return $this->render('user/profile.html.twig', ['user' => $user]);
+        if (!$this->getUser()) {
+            return $this->redirectToRoute('app_login');
+        }
+        return $this->render('user/profile.html.twig', ['user' => $this->getUser()]);
     }
 }
