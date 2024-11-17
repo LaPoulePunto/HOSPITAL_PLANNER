f7c64524495ab1d7bd8a4f344fbbb9e7caa732df
mart0495 <thibault.martin1@etudiant.univ-reims.fr>
2024-11-17 02:07:48 +0100

    Ajout de la méthode profile qui affiche les données de l'utilisateur connecté

--- src/Controller/UserController.php
+++ src/Controller/UserController.php
@@ -2,6 +2,7 @@
 
 namespace App\Controller;
 
+use App\Entity\User;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
@@ -14,5 +15,11 @@ class UserController extends AbstractController
         return $this->render('user/index.html.twig', [
             'controller_name' => 'UserController',
         ]);
+    }
+
+    #[Route('/user/{id}', name: 'app_user_profile')]
+    public function profile(User $user): Response
+    {
+        return $this->render('user/profile.html.twig', ['user' => $user]);
     }
 }
