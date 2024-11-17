0c6773d2e39e8f419dd2252240372b092a8293f4
mart0495 <thibault.martin1@etudiant.univ-reims.fr>
2024-11-17 17:27:46 +0100

    Ajout d'une route et d'une action de contrôleur pour afficher le dossier patient d'une consultation spécifique

--- src/Controller/HealthProfessionalController.php
+++ src/Controller/HealthProfessionalController.php
@@ -2,6 +2,7 @@
 
 namespace App\Controller;
 
+use App\Repository\ConsultationRepository;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
@@ -14,5 +15,21 @@ class HealthProfessionalController extends AbstractController
         return $this->render('health_professional/index.html.twig', [
             'controller_name' => 'HealthProfessionalController',
         ]);
+    }
+
+    #[Route('/health-professional/consultation/{consultationId}', name: 'app_display_patient_file')]
+    public function displayPatientFile(ConsultationRepository $consultationRepository, int $consultationId): Response
+    {
+        $consultation = $consultationRepository->getConsultationById($consultationId);
+        if (!$consultation) {
+            throw $this->createNotFoundException("Aucune consultation trouvée pour l'ID spécifié.");
+        }
+
+        $patient = $consultation->getPatient();
+        if (!$patient) {
+            throw $this->createNotFoundException("Aucun patient n'est associé à cette consultation.");
+        }
+
+        return $this->render('health_professional/displayPatientFile.html.twig', ['patient' => $patient]);
     }
 }
