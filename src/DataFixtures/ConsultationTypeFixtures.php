5cf826fafe6be4a70ddab880ab9745fc485076da
Nathan Villette <nathan.villette@etudiant.univ-reims.fr>
2024-11-20 19:01:19 +0100

    Création et configuration de la génération de données pour ConsulationType

--- src/DataFixtures/ConsultationTypeFixtures.php
+++ src/DataFixtures/ConsultationTypeFixtures.php
@@ -2,18 +2,18 @@
 
 namespace App\DataFixtures;
 
-use App\Factory\RoomTypeFactory;
+use App\Factory\ConsultationTypeFactory;
 use Doctrine\Bundle\FixturesBundle\Fixture;
 use Doctrine\Persistence\ObjectManager;
 
-class RoomTypeFixtures extends Fixture
+class ConsultationTypeFixtures extends Fixture
 {
     public function load(ObjectManager $manager): void
     {
         $content = json_decode(
-            file_get_contents(__DIR__.DIRECTORY_SEPARATOR.'data'.DIRECTORY_SEPARATOR.'RoomType.json'), true);
+            file_get_contents(__DIR__.DIRECTORY_SEPARATOR.'data'.DIRECTORY_SEPARATOR.'ConsultationType.json'), true);
         foreach ($content as $type) {
-            RoomTypeFactory::createOne($type);
+            ConsultationTypeFactory::createOne($type);
         }
     }
 }
