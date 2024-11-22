d41b2e6866e2c807b8473c3989d5a63e84fe9399
Tom Mairet <tom.mairet@etudiant.univ-reims.fr>
2024-11-22 18:13:20 +0100

    modification de la route read dans le controlleur UserController.php

--- src/Controller/UserController.php
+++ src/Controller/UserController.php
@@ -2,6 +2,7 @@
 
 namespace App\Controller;
 
+use App\Entity\User;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
@@ -28,10 +29,10 @@ class UserController extends AbstractController
     }
 
     #[Route('/user/update', name: 'app_user_update')]
-    public function read(): Response
+    public function read(User $user): Response
     {
         return $this->render('user/update.html.twig', [
-            'user' => $this->getUser(),
+            'user' => $user,
         ]);
     }
 }
