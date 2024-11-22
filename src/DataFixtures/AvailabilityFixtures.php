09ac0c61f5fa7c99bebdb85b7fa86555bd4b6271
oscar <oscar.neveux@etudiant.univ-reims.fr>
2024-11-22 17:14:26 +0100

    Création de la fixture pour pour Availability

---
+++ src/DataFixtures/AvailabilityFixtures.php
@@ -0,0 +1,69 @@
+<?php
+
+namespace App\DataFixtures;
+
+use App\Entity\Consultation;
+use App\Entity\HealthProfessional;
+use App\Factory\AvailabilityFactory;
+use DateTime;
+use Doctrine\Bundle\FixturesBundle\Fixture;
+use Doctrine\Common\DataFixtures\DependentFixtureInterface;
+use Doctrine\Persistence\ObjectManager;
+
+class AvailabilityFixtures extends Fixture implements DependentFixtureInterface
+{
+    public function load(ObjectManager $manager): void
+    {
+        $availabilityDict = [];
+        AvailabilityFactory::createMany(
+            100,
+            static function () use ($manager) {
+                $healthprofessionalId = rand(1, 10);
+                $healthprofessional = $manager->getRepository(HealthProfessional::class)->find($healthprofessionalId);
+                if (!isset($availabilityDict[$healthprofessionalId])) {
+                    $availabilityDict[$healthprofessionalId] = $manager->getRepository(Consultation::class)
+                        ->getAllConsultationsByHealthProfessionalId($healthprofessionalId);
+                }
+                $date = null;
+                while ($date === null) {
+                    $date = new DateTime();
+                    $date->modify('+' . rand(0, 29) . ' days')->format('Y-m-d');
+                    $startTime = new DateTime();
+                    $minutes = [0, 15, 30, 45];
+                    $startTime->setTime(rand(8, 20), $minutes[array_rand($minutes)]);
+                    $endTime = clone $startTime;
+                    $endTime->modify('+30 minutes');
+                    // On vérifie à chaque itération qu'il n'y ait pas de chevauchements
+                    foreach ($availabilityDict[$healthprofessionalId] as $dateTaken) {
+                        // Vérification qu'il n'y a pas de chevauchements
+                        if ($dateTaken['date'] === $date
+                            &&
+                            ($dateTaken['startTime'] >= $startTime && $dateTaken['startTime'] <= $endTime)
+                            ||
+                           ($dateTaken['endTime'] <= $endTime && $dateTaken['endTime'] >= $startTime)
+                            ||
+                            ($dateTaken['startTime']) <= $startTime && $dateTaken['endTime'] >= $endTime
+                        ) {
+                            // S'il y a un chevauchement, on refait un tour de boucle
+                            $date = null;
+                        }
+                    }
+                }
+
+                return [
+                    'healthprofessional' => $healthprofessional,
+                    'date' => $date,
+                    'startTime' => $startTime,
+                    'endTime' => $endTime
+                ];
+            }
+        );
+    }
+
+    public function getDependencies(): array
+    {
+        return [
+            HealthProfessionalFixtures::class
+        ];
+    }
+}
