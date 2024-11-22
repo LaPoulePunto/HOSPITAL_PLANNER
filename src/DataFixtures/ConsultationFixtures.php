e2bd2f03d302a2e6ed03d8f0649618af7821443a
oscar <oscar.neveux@etudiant.univ-reims.fr>
2024-11-22 17:10:32 +0100

    Modification de la date de fin des consultations factices et cs-fixer

--- src/DataFixtures/ConsultationFixtures.php
+++ src/DataFixtures/ConsultationFixtures.php
@@ -14,14 +14,16 @@ class ConsultationFixtures extends Fixture implements DependentFixtureInterface
 {
     public function load(ObjectManager $manager): void
     {
-        ConsultationFactory::createMany(10,
+        ConsultationFactory::createMany(
+            10,
             static function () use ($manager) {
                 $room = $manager->getRepository(Room::class)->find(rand(1, 10));
                 $consType = $manager->getRepository(ConsultationType::class)->find(rand(1, 10));
                 $patient = $manager->getRepository(Patient::class)->find(rand(1, 10));
 
                 return ['room' => $room, 'consultationtype' => $consType, 'patient' => $patient];
-            });
+            }
+        );
     }
 
     public function getDependencies(): array
