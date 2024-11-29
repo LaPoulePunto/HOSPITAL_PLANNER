75cb5d907859e8757c3d00b3a1fa4e2923a9d40d
Tom Mairet <tom.mairet@etudiant.univ-reims.fr>
2024-11-29 18:35:09 +0100

    Modification de l'action update pour mettre à jour en BD les infos contenues dans le formulaire, problème lorsque le mot de passe est vide -> il devient vide en BD

--- src/Controller/UserController.php
+++ src/Controller/UserController.php
@@ -3,8 +3,12 @@
 namespace App\Controller;
 
 use App\Entity\User;
+use App\Form\UserType;
+use Doctrine\ORM\EntityManagerInterface;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Symfony\Component\HttpFoundation\Request;
 use Symfony\Component\HttpFoundation\Response;
+use Symfony\Component\PasswordHasher\Hasher\UserPasswordHasherInterface;
 use Symfony\Component\Routing\Annotation\Route;
 
 class UserController extends AbstractController
@@ -23,16 +27,37 @@ class UserController extends AbstractController
         if (!$this->getUser()) {
             return $this->redirectToRoute('app_login');
         }
+
         return $this->render('user/profile.html.twig', [
             'user' => $this->getUser(),
         ]);
     }
 
-    #[Route('/user/update/{id}', name: 'app_user_update', requirements: ['id' => '\d+'])]
-    public function read(User $user): Response
+    #[Route('/user/{id}/update', name: 'app_user_update', requirements: ['id' => '\d+'])]
+    public function update(Request $request, User $user, UserPasswordHasherInterface $userPasswordHasher, EntityManagerInterface $entityManager): Response
     {
+        $form = $this->createForm(UserType::class, $user);
+        $form->handleRequest($request);
+
+        if ($form->isSubmitted() && $form->isValid()) {
+            if ($form->has('password') && $form->get('password')->getData()) {
+                var_dump($form->get('password')->getData());
+                $user->setPassword(
+                    $userPasswordHasher->hashPassword($user, $form->get('password')->getData())
+                );
+            }
+
+            $entityManager->persist($user);
+            $entityManager->flush();
+
+            $this->addFlash('success', 'Les informations de l\'utilisateur ont été mises à jour.');
+
+            return $this->redirectToRoute('app_user_update', ['id' => $user->getId()]);
+        }
+
         return $this->render('user/update.html.twig', [
             'user' => $user,
+            'form' => $form->createView(),
         ]);
     }
 }
