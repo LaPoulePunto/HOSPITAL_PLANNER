78efbdb5a4aa7bd8bf2fd9ffaaff2bb43b7b23db
Oscar Neveux <oscar.neveux@etudiant.univ-reims.fr>
2024-12-11 12:06:50 +0100

    Modification de la déclaration des variables dans l'action appointement avec la nouvelle méthode de ConsultationRepository.php

--- src/Controller/PatientController.php
+++ src/Controller/PatientController.php
@@ -2,7 +2,7 @@
 
 namespace App\Controller;
 
-use App\Repository\PatientRepository;
+use App\Repository\ConsultationRepository;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
@@ -12,20 +12,18 @@ class PatientController extends AbstractController
     #[Route('/patient', name: 'app_patient')]
     public function index(): Response
     {
-        return $this->render('patient/index.html.twig', [
-            'controller_name' => 'PatientController',
-        ]);
+        return $this->render('patient/index.html.twig');
     }
 
     #[Route('/user/appointment', name: 'app_user_show')]
-    public function appointment(PatientRepository $patientRepository): Response
+    public function appointment(ConsultationRepository $consultationRepository): Response
     {
         if (!$this->getUser()) {
             return $this->redirectToRoute('app_login');
         }
         $user = $this->getUser();
-        $futurAppointments = $patientRepository->findByIdPastOrFuturReservation($user->getId(), true);
-        $pastAppointments = $patientRepository->findByIdPastOrFuturReservation($user->getId(), false);
+        $futurAppointments = $consultationRepository->findByPatientPastOrFuturReservation($user, true);
+        $pastAppointments = $consultationRepository->findByPatientPastOrFuturReservation($user, false);
 
         return $this->render('patient/appointment.html.twig', [
             'futurAppointments' => $futurAppointments,
