3d238ea38ba0ff2582ab1f11f086dc4ab103a019
Nathan Villette <nathan.villette@etudiant.univ-reims.fr>
2024-12-18 11:16:34 +0100

    Création du fichier de test pour le login

---
+++ tests/Controller/SecurityController/loginCest.php
@@ -0,0 +1,17 @@
+<?php
+
+namespace App\Tests\Controller\SecurityController;
+
+use App\Tests\Support\ControllerTester;
+
+class LoginCest
+{
+    public function _before(ControllerTester $I)
+    {
+    }
+
+    // tests
+    public function tryToTest(ControllerTester $I)
+    {
+    }
+}
