4d126bb0d1caea0efdc3ce1320672f174fa3c0e1
Nathan Villette <nathan.villette@etudiant.univ-reims.fr>
2024-12-18 11:38:49 +0100

    Création du fichier de test pour le register

---
+++ tests/Controller/RegistrationController/RegisterCest.php
@@ -0,0 +1,17 @@
+<?php
+
+namespace App\Tests\Controller\RegistrationController;
+
+use App\Tests\Support\ControllerTester;
+
+class RegisterCest
+{
+    public function _before(ControllerTester $I)
+    {
+    }
+
+    // tests
+    public function tryToTest(ControllerTester $I)
+    {
+    }
+}
