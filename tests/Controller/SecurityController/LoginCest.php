873df02c4be6bf3da6dad5cdf588afdb0e670578
Nathan Villette <nathan.villette@etudiant.univ-reims.fr>
2024-12-18 11:31:03 +0100

    Modification du twig de login + écriture du test de connexion

---
+++ tests/Controller/SecurityController/LoginCest.php
@@ -0,0 +1,30 @@
+<?php
+
+namespace App\Tests\Controller\SecurityController;
+
+use App\Factory\PatientFactory;
+use App\Tests\Support\ControllerTester;
+
+class LoginCest
+{
+    public function _before(ControllerTester $I): void
+    {
+        PatientFactory::createOne([
+            'email' => 'test@example.com',
+        ]);
+    }
+
+    public function loginWithValidCredential(ControllerTester $I)
+    {
+        $I->amOnPage('/login');
+        $I->seeResponseCodeIsSuccessful();
+
+        $I->submitForm('form[name="login_form"]', [
+            'email' => 'test@example.com',
+            'password' => 'password',
+        ]);
+
+        $I->seeInCurrentUrl('/');
+        $I->seeResponseCodeIsSuccessful();
+    }
+}
