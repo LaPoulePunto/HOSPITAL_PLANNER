0a52bee09e8cbc00e4551bd10a4a98d657f37331
Nathan Villette <nathan.villette@etudiant.univ-reims.fr>
2024-12-24 12:10:57 +0100

    Ajout du test de création réussi d'un compte + modification du test de redirection

--- tests/Controller/Registration/RegisterCest.php
+++ tests/Controller/Registration/RegisterCest.php
@@ -2,6 +2,7 @@
 
 namespace App\Tests\Controller\Registration;
 
+use App\Entity\User;
 use App\Factory\PatientFactory;
 use App\Tests\Support\ControllerTester;
 
@@ -15,6 +16,34 @@ class RegisterCest
         $realUser = $user->_real();
         $I->amLoggedInAs($realUser);
         $I->amOnPage('/register');
-        $I->seeCurrentUrlEquals('/');
+        $I->seeCurrentRouteIs('app_home');
+    }
+
+    public function successfulRegister(ControllerTester $I): void
+    {
+        $I->amOnPage('/register');
+        $I->seeResponseCodeIsSuccessful();
+
+        $I->selectOption('registration_form[gender]', '1');
+        $I->checkOption('registration_form[agreeTerms]');
+
+        $I->fillField('registration_form[email]', 'test@example.com');
+        $I->fillField('registration_form[plainPassword][first]', 'Password');
+        $I->fillField('registration_form[plainPassword][second]', 'Password');
+        $I->fillField('registration_form[firstname]', 'Tom');
+        $I->fillField('registration_form[lastname]', 'Mairet');
+        $I->fillField('registration_form[birthDate]', '2000-01-01');
+        $I->fillField('registration_form[city]', 'Reims');
+        $I->fillField('registration_form[postCode]', '51100');
+        $I->fillField('registration_form[address]', '2 chemin des Rouliers');
+        $I->fillField('registration_form[phone]', '0601020304');
+
+        $I->selectOption('registration_form[gender]', '1');
+        $I->checkOption('registration_form[agreeTerms]');
+
+        $I->click('Créer le compte');
+
+        $I->seeInCurrentUrl('/');
+        $I->seeInRepository(User::class, ['email' => 'test@example.com']);
     }
 }
