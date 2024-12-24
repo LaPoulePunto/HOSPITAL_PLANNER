804fa0fd32c91eecfe852ffb062f6ffe5bbd9cf0
oscar <oscar.neveux@etudiant.univ-reims.fr>
2024-12-24 14:27:09 +0100

    Création des tests pour la route d'affichage des consultations pour un professionel de santé

--- tests/Controller/HealthProfessional/CalendarCest.php
+++ tests/Controller/HealthProfessional/CalendarCest.php
@@ -5,41 +5,63 @@ namespace App\Tests\Functional;
 use App\Entity\Consultation;
 use App\Entity\HealthProfessional;
 use App\Factory\ConsultationFactory;
+use App\Factory\ConsultationTypeFactory;
 use App\Factory\HealthProfessionalFactory;
+use App\Factory\MaterialFactory;
+use App\Factory\PatientFactory;
+use App\Factory\RoomFactory;
+use App\Factory\RoomTypeFactory;
 use App\Tests\Support\ControllerTester;
 
 class HealthProfessionalCalendarCest
 {
-    private HealthProfessional $healthProfessional;
-    private Consultation  $consultation;
-
     public function _before(ControllerTester $I)
     {
-        // Création d'un utilisateur avec une factory
-        $this->healthProfessional = HealthProfessionalFactory::createOne([
-            'email' => 'test@hp.com',
-            'password' => 'password',
-        ])->_real();
-        $I->amLoggedInAs($this->healthProfessional);
+        $healthProfessional = HealthProfessionalFactory::createOne()->_real();
+        $I->amLoggedInAs($healthProfessional);
 
-        // Création de la consultation
         $startTime = new \DateTime();
         $startTime->setTime(9, 0);
         $endTime = clone $startTime;
         $endTime->modify('+1 hour');
 
-        $this->consultation = ConsultationFactory::createOne([
+        $roomType = RoomTypeFactory::createOne([
+            'label' => 'Salle de Radiologie'
+        ])->_real();
+
+        $room = RoomFactory::createOne([
+            'floor' => 0,
+            'num' => 10,
+            'roomtype' => $roomType
+        ])->_real();
+
+        $consultationType = ConsultationTypeFactory::createOne([
+            'label' => 'Kinésithérapie'
+        ])->_real();
+
+        $patient = PatientFactory::createOne([
+            'lastname' => 'Martin',
+            'firstname' => 'Thibault',
+        ])->_real();
+
+        $consultation = ConsultationFactory::createOne([
             'date' => new \DateTime('2013-05-23'),
             'start_time' => $startTime,
             'end_time' => $endTime,
+            'room' => $room,
+            'consultationType' => $consultationType,
+            'patient' => $patient,
         ])->_real();
 
-        $this->consultation->addHealthprofessional($this->healthProfessional);
-
+        $consultation->addHealthprofessional($healthProfessional);
         $entityManager = $I->grabService('doctrine.orm.entity_manager');
 
-        $entityManager->persist($this->consultation);
-        $entityManager->persist($this->healthProfessional);
+        $entityManager->persist($consultation);
+        $entityManager->persist($healthProfessional);
+        $entityManager->persist($room);
+        $entityManager->persist($roomType);
+        $entityManager->persist($consultationType);
+        $entityManager->persist($patient);
         $entityManager->flush();
     }
 
@@ -56,7 +78,8 @@ class HealthProfessionalCalendarCest
     public function CalendarDisplaysAppointments(ControllerTester $I): void
     {
         $I->amOnPage('/health-professional/calendar');
+        $I->seeInSource("title: 'PATIENT : MARTIN Thibault | TYPE : Kinésithérapie | SALLE : 10 | ÉTAGE : 0 | TYPE DE SALLE : Salle de Radiologie'");
         $I->seeInSource("start: '2013-05-23T09:00:00'");
-        //        , end: '2013-05-23T10:00:00
+        $I->seeInSource("end: '2013-05-23T10:00:00'");
     }
 }
