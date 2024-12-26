d7b9d1c807754143b40e2c1100ae4c819ad59942
oscar <oscar.neveux@etudiant.univ-reims.fr>
2024-12-26 22:14:37 +0100

    Ajout de la gestion de l'envoi du formulaire de l'action create et update

--- src/Controller/AvailabilityController.php
+++ src/Controller/AvailabilityController.php
@@ -4,9 +4,12 @@ namespace App\Controller;
 
 use App\Entity\Availability;
 use App\Form\AvailabilityType;
+use Doctrine\ORM\EntityManagerInterface;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Symfony\Component\HttpFoundation\Request;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
+use Symfony\Component\Security\Http\Attribute\IsGranted;
 
 class AvailabilityController extends AbstractController
 {
@@ -17,20 +20,38 @@ class AvailabilityController extends AbstractController
     }
 
     #[Route('/availability/create', name: 'app_availability_create')]
-    public function create(): Response
+    public function create(Request $request, EntityManagerInterface $entityManager): Response
     {
         $availability = new Availability();
+        $availability->setHealthprofessional($this->getUser());
         $form = $this->createForm(AvailabilityType::class, $availability);
+        $form->handleRequest($request);
+        if ($form->isSubmitted() && $form->isValid()) {
+            try {
+                $entityManager->persist($availability);
+                $entityManager->flush();
 
+            } catch (\Exception) {
+                echo 'Erreur de création de la disponibilité';
+            }
+        }
         return $this->render('availability/create.html.twig', [
             'form' => $form,
         ]);
     }
 
     #[Route('/availability/{id}/update', name: 'app_availability_update')]
-    public function update(Availability $availability): Response
+    public function update(Availability $availability, Request $request, EntityManagerInterface $entityManager): Response
     {
         $form = $this->createForm(AvailabilityType::class, $availability);
+        $form->handleRequest($request);
+        if ($form->isSubmitted() && $form->isValid()) {
+            try {
+                $entityManager->flush();
+            } catch (\Exception) {
+                echo 'Erreur de modification de la disponibilité';
+            }
+        }
 
         return $this->render('availability/update.html.twig', [
             'form' => $form,
