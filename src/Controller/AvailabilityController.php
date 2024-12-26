cd7f9c5c3ca11f2fb7ad6da77c10f64f0193016b
oscar <oscar.neveux@etudiant.univ-reims.fr>
2024-12-26 16:20:07 +0100

    Création des routes du crud avec leurs controller associer

--- src/Controller/AvailabilityController.php
+++ src/Controller/AvailabilityController.php
@@ -13,4 +13,22 @@ class AvailabilityController extends AbstractController
     {
         return $this->render('availability/index.html.twig');
     }
+
+    #[Route('/availability', name: 'app_availability_create')]
+    public function create(): Response
+    {
+        return $this->render('availability/create.html.twig');
+    }
+
+    #[Route('/availability/{id}', name: 'app_availability_update')]
+    public function update(): Response
+    {
+        return $this->render('availability/update.html.twig');
+    }
+
+    #[Route('/availability/{id}', name: 'app_availability_delete')]
+    public function delete(): Response
+    {
+        return $this->render('delete/index.html.twig');
+    }
 }
