12208c7b401eaaaa7a5a8602e66d986f45b0ddf8
Nathan Villette <nathan.villette@etudiant.univ-reims.fr>
2024-12-29 12:46:44 +0100

    Modification de la redirection après login + correction du test

--- src/Security/LoginFormAuthenticator.php
+++ src/Security/LoginFormAuthenticator.php
@@ -48,7 +48,7 @@ class LoginFormAuthenticator extends AbstractLoginFormAuthenticator
             return new RedirectResponse($targetPath);
         }
 
-        return new RedirectResponse($this->urlGenerator->generate('app_home'));
+        return new RedirectResponse($this->urlGenerator->generate('app_home_user'));
     }
 
     protected function getLoginUrl(Request $request): string
