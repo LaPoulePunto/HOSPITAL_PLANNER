e5a4f70c2ef3722e0848368ba8a12cdbce18a867
Nathan Villette <nathan.villette@etudiant.univ-reims.fr>
2025-01-06 20:26:08 +0100

    Ajout du test de création d'une consultation en tant que patient

--- tests/Controller/Consultation/CreateCest.php
+++ tests/Controller/Consultation/CreateCest.php
@@ -2,17 +2,58 @@
 
 namespace App\Tests\Controller\Consultation;
 
+use App\Entity\ConsultationType;
+use App\Entity\HealthProfessional;
+use App\Entity\Patient;
+use App\Factory\ConsultationTypeFactory;
+use App\Factory\HealthProfessionalFactory;
+use App\Factory\PatientFactory;
 use App\Tests\Support\ControllerTester;
 
 class CreateCest
 {
+    private Patient $patient;
+    private HealthProfessional  $healthProfessional;
+    private ConsultationType $consultationType;
+
     public function _before(ControllerTester $I)
     {
+        $this->patient = PatientFactory::createOne()->_real();
+        $this->healthProfessional = HealthProfessionalFactory::createOne()->_real();
+        $this->consultationType = ConsultationTypeFactory::createOne()->_real();
     }
 
     public function testRedirectToLoginIfNotAuthenticated(ControllerTester $I)
     {
         $I->amOnPage('/consultation/create');
         $I->seeCurrentRouteIs('app_login');
+    }
+
+    public function createConsultationAsPatient(ControllerTester $I)
+    {
+        $I->amLoggedInAs($this->patient);
+        $I->amOnPage('/consultation/create');
+        $I->seeResponseCodeIsSuccessful();
+
+        // Choisir la date
+        $I->selectOption('consultation_form[date][day]', '15');
+        $I->selectOption('consultation_form[date][month]', '5');
+        $I->selectOption('consultation_form[date][year]', '2025');
+
+        // Heure de début
+        $I->selectOption('consultation_form[startTime][hour]', '10');
+        $I->selectOption('consultation_form[startTime][minute]', '30');
+
+        // Heure de fin
+        $I->selectOption('consultation_form[endTime][hour]', '11');
+        $I->selectOption('consultation_form[endTime][minute]', '30');
+
+        // Type de consultation
+        $I->selectOption('consultation_form[consultationType]', $this->consultationType->getLabel());
+
+        // Soumettre le formulaire
+        $I->click('input[type=submit][value=Création]');
+
+        $I->seeCurrentRouteIs('app_consultation_select_health_professional');
     }
 }
