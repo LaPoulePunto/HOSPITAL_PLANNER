85f5552d312f8f917b8e79b6fda7c4f7e4121dc3
Allan <allan.laheu@etudiant.univ-reims.fr>
2025-01-06 21:39:45 +0100

    ajout d'un test vérifiant la redirection d'un professionnel de santé vers sa page d'accueil

---
+++ tests/Controller/Home/HomeUserCest.php
@@ -0,0 +1,26 @@
+<?php
+
+namespace App\Tests\Controller\Home;
+
+use App\Factory\HealthProfessionalFactory;
+use App\Tests\Support\ControllerTester;
+
+class HomeUserCest
+{
+    private object $healthProfessional;
+
+    public function _before(ControllerTester $I)
+    {
+        $this->healthProfessional = HealthProfessionalFactory::createOne()->_real();
+    }
+
+    // tests
+    public function redirectingToHomeAsHealthProfessional(ControllerTester $I)
+    {
+        $I->amLoggedInAs($this->healthProfessional);
+        $I->amOnPage('/health-professional/calendar');
+        $I->seeResponseCodeIsSuccessful();
+        $I->click('HOSPITIME');
+        $I->seeCurrentRouteIs('app_home_user');
+    }
+}
