ce55344ddc6b5bf13351d012c2e4ef66a217cf8e
mart0495 <thibault.martin1@etudiant.univ-reims.fr>
2025-01-17 00:58:38 +0100

    Modifications du champ etage en choice field

--- src/Controller/Admin/RoomCrudController.php
+++ src/Controller/Admin/RoomCrudController.php
@@ -3,7 +3,9 @@
 namespace App\Controller\Admin;
 
 use App\Entity\Room;
+use Doctrine\ORM\EntityManagerInterface;
 use EasyCorp\Bundle\EasyAdminBundle\Controller\AbstractCrudController;
+use EasyCorp\Bundle\EasyAdminBundle\Field\ChoiceField;
 use EasyCorp\Bundle\EasyAdminBundle\Field\Field;
 use EasyCorp\Bundle\EasyAdminBundle\Field\IdField;
 use EasyCorp\Bundle\EasyAdminBundle\Field\TextEditorField;
@@ -11,19 +13,37 @@ use EasyCorp\Bundle\EasyAdminBundle\Field\TextField;
 
 class RoomCrudController extends AbstractCrudController
 {
+    private EntityManagerInterface $entityManager;
+
+    public function __construct(EntityManagerInterface $entityManager)
+    {
+        $this->entityManager = $entityManager;
+    }
+
     public static function getEntityFqcn(): string
     {
         return Room::class;
     }
 
-
     public function configureFields(string $pageName): iterable
     {
+        $floors = $this->entityManager->createQueryBuilder()
+            ->select('DISTINCT r.floor')
+            ->from(Room::class, 'r')
+            ->getQuery()
+            ->getResult();
+
+        $floorChoices = [];
+        foreach ($floors as $floor) {
+            $floorChoices[$floor['floor']] = $floor['floor'];
+        }
+
         return [
             IdField::new('id')->hideOnForm(),
             Field::new('num', 'Numéro'),
-            TextField::new('floor', 'Étage'),
+            ChoiceField::new('floor', 'Etage')
+                ->setChoices($floorChoices)
+                ->setRequired(true)
         ];
     }
-
 }
